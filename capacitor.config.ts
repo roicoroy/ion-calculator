e76633af6e5580afce06be6c3bad9e2eb61caadd
Ricardo Watanabe Bento <roicoroy@yahoo.com.br>
2024-10-08 16:07:26 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ion-calculator',
+  webDir: 'www'
+};
+
+export default config;
